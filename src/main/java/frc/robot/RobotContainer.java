2b60c33f953136cfc5afd8de53d94af4edae4b38
Dixie Cooper <dcooper@explora.us>
2025-02-04 18:54:21 -0800

    Worked on motor tuning

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -21,6 +21,7 @@ import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.DriveTrainSubsystem;
 import frc.robot.subsystems.HangerSubsystem;
+import frc.robot.subsystems.LiftSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -42,7 +43,9 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
   private final DriveTrainSubsystem m_robotDrive = new DriveTrainSubsystem();
-  // private final HangerSubsystem m_robotHanger = new HangerSubsystem();
+  // private final HangerSubsystem m_robotHanger = new HangerSubsystem();\
+  private final LiftSubsystem m_robotLift = new LiftSubsystem();
+
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   //private final CommandXboxController m_driverController =
@@ -62,8 +65,8 @@ public class RobotContainer {
             () ->
                 m_robotDrive.drive(
                     -m_driverController.getLeftY(),
-                    -m_driverController.getRightX(),
-                    -m_driverController.getLeftX(),
+                    m_driverController.getRightX(),
+                    m_driverController.getLeftX(),
                     false),
             m_robotDrive));
   }
@@ -100,6 +103,12 @@ public class RobotContainer {
         // new JoystickButton(m_driverController, Button.kB.value)
         // .onTrue(m_robotHanger.reverseMotor())
         // .onFalse(m_robotHanger.stopMotor());
+
+        new JoystickButton(m_driverController, Button.kX.value)
+        .onTrue(m_robotLift.goToPosition());
+
+        new JoystickButton(m_driverController, Button.kY.value)
+        .onTrue(m_robotLift.encoderReset());
   }
 
   /**
--- src/main/java/frc/robot/subsystems/LiftSubsystem.java
+++ src/main/java/frc/robot/subsystems/LiftSubsystem.java
@@ -4,12 +4,88 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+
 public class LiftSubsystem extends SubsystemBase {
+  private final SparkMax m_testMotor = new SparkMax(3, MotorType.kBrushless);
+  private final SparkClosedLoopController closedLoopController = m_testMotor.getClosedLoopController();
+  private final RelativeEncoder encoder = m_testMotor.getEncoder();
+  private final SparkMaxConfig motorConfig = new SparkMaxConfig();
+
+
   /** Creates a new LiftSubsystem. */
-  public LiftSubsystem() {}
+  public LiftSubsystem() {
+    encoder.setPosition(0);
+    motorConfig.encoder
+      .positionConversionFactor(1)
+      .velocityConversionFactor(1);
+
+    motorConfig.closedLoop
+      .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+      // Set PID values for position control. We don't need to pass a closed loop
+      // slot, as it will default to slot 0.
+      .p(1.0)
+      .i(0)
+      .d(2.0)
+      .outputRange(-1, 1)
+      // Set PID values for velocity control in slot 1
+      .p(0.0001, ClosedLoopSlot.kSlot1)
+      .i(0, ClosedLoopSlot.kSlot1)
+      .d(0, ClosedLoopSlot.kSlot1)
+      .velocityFF(1.0 / 5767, ClosedLoopSlot.kSlot1)
+      .outputRange(-1, 1, ClosedLoopSlot.kSlot1);
+
+    m_testMotor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+
+    SmartDashboard.setDefaultNumber("Target Position", 0);
+    SmartDashboard.setDefaultNumber("Target Velocity", 0);
+    SmartDashboard.setDefaultBoolean("Control Mode", false);
+    SmartDashboard.setDefaultBoolean("Reset Encoder", false);
+
+
+  }
+
+  public Command goToPosition() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          closedLoopController.setReference(10.0, ControlType.kPosition, ClosedLoopSlot.kSlot0);
+          System.out.println("X was pressed");
+        });
+  }
+
+  public Command encoderReset() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          closedLoopController.setReference(0, ControlType.kPosition, ClosedLoopSlot.kSlot0);
+          encoder.setPosition(0);
+          closedLoopController.setReference(0, ControlType.kPosition, ClosedLoopSlot.kSlot0);
+          encoder.setPosition(0);
+          System.out.println("Y was pressed");
+        });
+  }
+
+
+
 
   /**
    * Example command factory method.
